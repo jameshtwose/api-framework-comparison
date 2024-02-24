e071ca337c3aae0bf23d56de4cea69e9d9cc5a1a
James Twose <james.twose2711@gmail.com>
2024-02-24 11:40:00 +0000

    Getting started on the PHP and .NET implementations, finishing off the R plumber-postgres one

---
+++ asp-entity/Program.cs
@@ -0,0 +1,27 @@
+var builder = WebApplication.CreateBuilder(args);
+// add the services for swagger
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+// add the postgresql connection
+// builder.AddNpgsqlDbContext<ApiDbContext>("postgresdb");
+
+var app = builder.Build();
+
+// add the endpoints
+// Get the root path of the app
+// it will return {"Hello": "Welcome to the ASP.NET - Entity API"}
+app.MapGet("/", () => Results.Ok(new { Hello = "Welcome to the ASP.NET - Entity API" }));
+
+// Get the first X rows from the complaints_table table
+// app.MapGet("/complaints", async (ApiDbContext dbContext) =>
+// {
+//     var complaints = await dbContext.complaints_table.Take(10).ToListAsync();
+//     return Results.Ok(complaints);
+// });
+
+// run swagger
+app.UseSwagger();
+app.UseSwaggerUI();
+// run the app
+app.Run();
---
+++ asp-entity/README.md
@@ -0,0 +1,11 @@
+### ASP.NET (8.0) + Entity Framework
+- Tutorial: https://learn.microsoft.com/en-us/aspnet/core/tutorials/min-web-api?view=aspnetcore-8.0&tabs=visual-studio-code
+- Initial setup: `dotnet new web -o asp-entity` (run once)
+- `cd asp-entity`
+- `dotnet add package Microsoft.EntityFrameworkCore.InMemory`
+- `dotnet add package Microsoft.AspNetCore.Diagnostics.EntityFrameworkCore`
+- Get the pgsql package compatible with .NET 8.0
+    - `dotnet add package Aspire.Npgsql.EntityFrameworkCore.PostgreSQL --version 8.0.0-preview.2.23619.3`
+- `dotnet add package Swashbuckle.AspNetCore`
+- `dotnet dev-certs https --trust` 
+- `dotnet run --launch-profile https`
